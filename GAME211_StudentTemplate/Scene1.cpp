9d3f94f4f2125df13dcb50c0657d222507c16fa6
Akamundi <145157267+Akamundi@users.noreply.github.com>
2024-09-17 16:50:00 -0400

    Chunk and Region Handling

--- GAME211_StudentTemplate/Scene1.cpp
+++ GAME211_StudentTemplate/Scene1.cpp
@@ -1,5 +1,8 @@
 #include "Scene1.h"
 #include <VMath.h>
+#include "Chunk.h"
+#include "ChunkHandler.h"
+
 
 // See notes about this constructor in Scene1.h.
 Scene1::Scene1(SDL_Window* sdlWindow_, GameManager* game_){
@@ -34,6 +37,21 @@ bool Scene1::OnCreate() {
 	game->getPlayer()->setImage(image);
 	game->getPlayer()->setTexture(texture);
 
+	ChunkHandler RegionOne();
+
+	std::vector<Vec2> changesIndex = { 
+		{0,0}, {0,1}, {0,2}, {0,3}, {0,4}, {0,5}, {0,6}, {0,7}, {0,8}, {0,9}, {0,10}, {0,11}, {0,12}, {0,13}, {0,14}, {0,15},
+        {1,0}, {1,1}, {1,2}, {1,3}, {1,4}, {1,5}, {1,6}, {1,7}, {1,8}, {1,9}, {1,10}, {1,11}, {1,12}, {1,13}, {1,14}, {1,-10} };
+	std::vector<int> changesValues = { 1, 1, 6, 7, 4, 5, 8, 4, 6, 8, 4, 3, 7, 8, 5, 3, 1, 1, 6, 7, 4, 5, 8, 4, 6, 8, 4, 3, 7, 8, 5, 3};
+	
+	Chunk testChunk(changesIndex, changesValues);
+
+	testChunk.printChunk();
+
+	testChunk.setTile(changesIndex, changesValues);
+
+	testChunk.printChunk();
+
 	return true;
 }
 
@@ -43,6 +61,8 @@ void Scene1::Update(const float deltaTime) {
 
 	// Update player
 	game->getPlayer()->Update(deltaTime);
+
+	
 }
 
 void Scene1::Render() {
