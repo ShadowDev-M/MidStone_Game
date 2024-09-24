8bd1d335cba6206fae67a1f5a357cc932e534c72
Malarkey <128517931+Malarkey1@users.noreply.github.com>
2024-09-24 01:53:19 -0400

    fixed inventory stuff
    made inventory a 2d array as a reference to the items class, but is default to all nullptr's.

--- GAME211_StudentTemplate/Inventory.h
+++ GAME211_StudentTemplate/Inventory.h
@@ -2,25 +2,14 @@
 #include "Item.h"
 class Inventory
 {
-	char hotkey1;
-	char hotkey2;
-	char hotkey3;
-	char hotkey4;
-	char hotkey5;
+	static const int rows = 4;
+	static const int cols = 5;
+	Item* mainInventory[rows][cols];
 
-	int maxLoad = 20;
-	const char* mainInventory[5];
-	const char* extraInventory[15];
 
 public:
-	
-	Inventory()
-	{
-		mainInventory[0] = &hotkey1;
-		mainInventory[1] = &hotkey2;
-		mainInventory[2] = &hotkey3;
-		mainInventory[3] = &hotkey4;
-		mainInventory[4] = &hotkey5;
-	}
+	Inventory();
+	bool addItem(Item* item);
+	bool removeItem(int row, int col);	
 };
 
